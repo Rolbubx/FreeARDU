97a06e7d9d085c19bf1e33ecd2173d1c391b489b
Rolbubx <164780728+Rolbubx@users.noreply.github.com>
2026-07-09 17:05:07 +0200

    A new window controller (it's in beta though)

--- FreeARDU/src/on_start/entry_point/main.cpp
+++ FreeARDU/src/on_start/entry_point/main.cpp
@@ -1,23 +1,40 @@
 #ifdef FREEARDU_BARE_METAL
 #include "../../framebuffer/framebuffer.h"
 #include "../../GraphicalEntryDetector/graphical_entry_detector.h"
-
+#define CPU_FREQ 600000000
 extern Framebuffer framebuffer;
 
+
+class WaiterScheduler {
+public:
+    float transform_cycles_to_seconds(volatile unsigned int cycles) {
+        return (float)cycles / CPU_FREQ;
+    }
+
+    void wait_cycles(volatile unsigned int count) {
+        while (count > 0) {
+            count--;
+        }
+    }
+};
+
 extern "C" int main() {
-    // Initialize OS/Runtime components
+    // init the framebuffer
     framebuffer.INIT();
     
-    // Clear screen
+    // clear screen
     Color black = {0.0f, 0.0f, 0.0f};
     framebuffer.CLEAR(black);
     framebuffer.FLUSH();
 
     while (1) {
-        // Main OS Loop
+        // mains os loop
+
+
     }
     return 0;
 }
+
 #else
 #include <Arduino.h>
 
